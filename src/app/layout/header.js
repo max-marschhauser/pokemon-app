1f7510448f69de46a8d7127186fc7a2534b1e008
max-marschhauser <105870867+max-marschhauser@users.noreply.github.com>
2022-09-28 13:09:17 +0200

    added music

--- src/app/layout/header.js
+++ src/app/layout/header.js
@@ -3,6 +3,9 @@ import React from "react";
 export default function Header() {
 	return (
 		<header className="header">
+			<audio controls autoplay muted loop>
+				<source src="pokemonthemesong.mp3" />
+			</audio>
 			<div className="header__logo">
 				<span>&nbsp;Who's&nbsp;</span>
 				<span>&nbsp;that&nbsp;</span>
