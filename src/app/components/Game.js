44a05eae37ed436d23bedeeb7758170dbc477f4c
max-marschhauser <105870867+max-marschhauser@users.noreply.github.com>
2022-09-28 12:23:48 +0200

    mostly finished game

--- src/app/components/Game.js
+++ src/app/components/Game.js
@@ -1,23 +1,94 @@
 import React, { useState } from "react";
 import getPokemonName from "../utils/getPokemonName";
-import Options from "../components/Options";
+import getPokemonImage from "../utils/getPokemonImage";
+import getCorrectPokemonImage from "../utils/getCorrectPokemonImage";
+import GetPokemonName from "../utils/getPokemonName";
+
+let scoreCount = 0;
+let combo = 0;
 
 export default function Game() {
-	const [currentPokemon, setCurrentPokemon] = useState(getPokemonName);
-
+	const [correctPokemon, setCorrectPokemon] = useState(getPokemonName);
 	function newPokemon() {
-		setCurrentPokemon(getPokemonName);
+		setCorrectPokemon(getPokemonName);
+	}
+
+	let randNum = Math.floor(Math.random() * 4 + 1);
+	let correctPokemonSrc = getCorrectPokemonImage(correctPokemon);
+	let wrongPokemons = [];
+	let clickedPokemon = false;
+	let tryPokemon = "";
+
+	while (wrongPokemons.length < 3) {
+		tryPokemon = GetPokemonName();
+
+		if (tryPokemon !== correctPokemon && wrongPokemons.includes(tryPokemon) === false) {
+			wrongPokemons.push(tryPokemon);
+		}
+	}
+
+	function imageClicked(event) {
+		if (clickedPokemon === false) {
+			let pokemonImageSrc = event.target.src;
+			let correctPokemonImageSrc = getCorrectPokemonImage(correctPokemon);
+
+			if (pokemonImageSrc === correctPokemonImageSrc) {
+				combo++;
+				event.target.className = "answer--right";
+				clickedPokemon = true;
+				setTimeout(resetClassName, 1000);
+			} else {
+				combo = 0;
+				event.target.className = "answer--wrong";
+				clickedPokemon = true;
+				setTimeout(resetClassName, 1000);
+			}
+
+			if (combo > scoreCount) {
+				scoreCount = combo;
+			}
+		}
+
+		function resetClassName() {
+			event.target.className = "answer";
+			clickedPokemon = false;
+			newPokemon();
+		}
 	}
 
 	return (
 		<>
-			<div className="question">Which one is {currentPokemon}?</div>
+			<div className="question">Which one is {correctPokemon}?</div>
 			<div className="image--container">
-				<Options correctPokemon={currentPokemon} />
+				<img
+					className="answer"
+					src={randNum === 1 ? correctPokemonSrc : getPokemonImage(wrongPokemons.pop())}
+					alt="Error"
+					onClick={imageClicked}
+				/>
+				<img
+					className="answer"
+					src={randNum === 2 ? correctPokemonSrc : getPokemonImage(wrongPokemons.pop())}
+					alt="Error"
+					onClick={imageClicked}
+				/>
+				<img
+					className="answer"
+					src={randNum === 3 ? correctPokemonSrc : getPokemonImage(wrongPokemons.pop())}
+					alt="Error"
+					onClick={imageClicked}
+				/>
+				<img
+					className="answer"
+					src={randNum === 4 ? correctPokemonSrc : getPokemonImage(wrongPokemons.pop())}
+					alt="Error"
+					onClick={imageClicked}
+				/>
 			</div>
-			<button className="nextPokemon--button" onClick={newPokemon}>
-				Next Pokémon
-			</button>
+			<div className="score">
+				<span>&nbsp;Combo:&nbsp;{combo}&nbsp;</span>
+				<span>&nbsp;High-Score:&nbsp;{scoreCount}&nbsp;</span>
+			</div>
 		</>
 	);
 }
